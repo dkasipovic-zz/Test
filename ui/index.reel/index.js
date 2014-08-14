969060f351da8580573aecf557b533c6bc9b0dec
D. Kasipovic <noreply>
2014-08-14 13:04:10 +0000

    Add component index

---
+++ ui/index.reel/index.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/index.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Index
+ * @extends Component
+ */
+exports.Index = Component.specialize(/** @lends Index# */ {
+    constructor: {
+        value: function Index() {
+            this.super();
+        }
+    }
+});
